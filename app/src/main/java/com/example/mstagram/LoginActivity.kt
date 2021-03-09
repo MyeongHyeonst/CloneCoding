c888d9d807917dee4e53bf7290161da633fd7590
MyeongHyeonst <aud0420@ajou.ac.kr>
2021-03-09 17:40:00 +0900

    <component name="Mstagram"> <settings default="" /> </component>

---
+++ app/src/main/java/com/example/mstagram/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
